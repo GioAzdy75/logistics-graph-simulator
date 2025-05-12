25d8d513274a459bc75e52614f71fcb7aac5fb88
gabichulas <gabylopezromero13@gmail.com>
2025-05-12 20:48:40 -0300

    organizacion del codigo

--- consults.py
+++ consults.py
@@ -1,21 +1,4 @@
-import numpy as np
 import networkx as nx
-
-def get_poi_ids(driver):
-    query = """
-    MATCH (d:CentroDistribucion)-[:ACCESO_SALIDA]->(e:Esquina)
-    WITH collect(id(e)) AS depotEsquinas
-
-    MATCH (c:Cliente)<-[:ACCESO_ENTRADA]-(e2:Esquina)
-    WITH depotEsquinas, collect(id(e2)) AS clienteEsquinas
-
-    RETURN depotEsquinas + clienteEsquinas AS poiIds
-    """
-    with driver.session() as session:
-        result = session.run(query).single()
-        poi_ids = result["poiIds"]
-        return list(dict.fromkeys(poi_ids))  # Elimina duplicados manteniendo orden
-
 
 def build_node_index(poi_ids):
     return {nid: i for i, nid in enumerate(poi_ids)}
@@ -34,60 +17,12 @@ def get_nodes_by_ids(driver, ids):
     with driver.session() as session:
         result = session.run(query, ids=ids)
         return result.single()["nodes"]
-""" 
-def compute_distance_matrix_dijkstra(driver, poi_ids):
-    ""
-    Retorna una matriz de distancia minima evaluada por dijkstra entre los nodos, 
-    y una un diccionario con los caminos que conforman las distancia entre los nodos antes planteados
-    ""
-    CY_DIJKSTRA_PATHS = ""
-    MATCH (start:Intersection) WHERE id(start) = $source
-    CALL gds.shortestPath.dijkstra.stream('mapa-logistico', {
-      sourceNode: id(start),
-      targetNodes: $targets,
-      relationshipWeightProperty: 'length'
-    })
-    YIELD targetNode, totalCost, nodeIds
-    UNWIND nodeIds AS nid
-    WITH targetNode, totalCost, gds.util.asNode(nid) AS node
-    RETURN 
-      targetNode AS target_id,
-      totalCost,
-      collect({
-        id: node.id,
-        lon: node.lon,
-        lat: node.lat
-      }) AS path
-    ""
-
-    n = len(poi_ids)
-    dist = np.full((n, n), np.inf)
-    np.fill_diagonal(dist, 0.0)
-    node_idx = build_node_index(poi_ids)
-    paths = {}
-
-    with driver.session() as session:
-        for src_id in poi_ids:
-            result = session.run(CY_DIJKSTRA_PATHS, source=src_id, targets=poi_ids)
-            i = node_idx[src_id]
-            for record in result:
-                tgt_id = record["target_id"]
-                cost = record["totalCost"]
-                path = record["path"]
-
-                if tgt_id in node_idx:
-                    j = node_idx[tgt_id]
-                    dist[i, j] = cost
-                    paths[(src_id, tgt_id)] = path
-
-    return dist, paths
- """
 
 def extract_graph_data(driver):
     """
     Extrae los datos del grafo desde Neo4j y los convierte en un grafo de NetworkX.
     """
-    G = nx.DiGraph()  # Cambia a nx.Graph() si el grafo no es dirigido
+    G = nx.DiGraph()
 
     with driver.session() as session:
         # Extraer nodos
--- main.py
+++ main.py
@@ -1,14 +1,7 @@
-import numpy as np
-import pandas as pd
 import config
 from neo4j import GraphDatabase
-
-
 import graph_map
-
-
-
-from consults import get_poi_ids,get_nodes_by_ids, extract_graph_data
+from consults import get_nodes_by_ids, extract_graph_data
 
 URI      = config.URI # Default : "bolt://localhost:7687"
 USER     = config.USER # Default : "neo4j"
@@ -16,48 +9,31 @@ PASSWORD = config.PASSWORD #
 
 driver = GraphDatabase.driver(URI, auth=(USER, PASSWORD))
 
-#Nodos de Interes
-#ids_nodos = [4801, 187, 17258, 61] # Source Ids
-#ids_nodos = [1135846205, 480124288, 6064229614, 287055452]
-#Calculamos las distancias
-""" 
-dist_matrix, paths = compute_distance_matrix_dijkstra(driver, ids_nodos)
-print("Matriz de distancias (Dijkstra):")
-print(dist_matrix)
-
-
-# Guardar la matriz
-np.save("dist_matrix.npy", dist_matrix)
-print("Matriz de distancias guardada como dist_matrix.npy")
-
-
-#Imprimir todos los caminos
-#print(paths[(4801,187)])
-print(paths)
-graph_map.create_graph_map_from_paths(paths)
- """
-
-#Ejecutamos Optimizacion
-###Se ejectuario el algoritmo de optimizacion y nos devolveria el orden de busqueda del nodo ej [node1,node3,node2,node4]
-# luego con eso hay que crear una lista de la forma  [(node1,node3),(node3,node2),...] 
+# Extraemos el grafo para operar internamente sobre el
 graph = extract_graph_data(driver)
 
-from algorithms import moaco, reformat_path
+from algorithms import moaco
 
+# Obtenemos la lista correspondiente al camino más optimo encontrado por MOACO
 optimal_path = moaco(graph)[0]
 
-
-
+"""Convertimos la lista obtenida en un diccionario con el formato:
+    {(nodo_1,nodo_2) : [{"id": 4801, "lon": -68.8205003, "lat": -32.9040337}
+                        {"id": 4792, "lon": -68.8205776, "lat": -32.9046553},
+                        {"id": 4780, "lon": -68.8206100, "lat": -32.9051000},
+                        ...
+                       ],
+     (nodo_5,nodo_9) : [...],
+     (nodo_3,nodo_7) : [...],
+     ...
+    }
+"""
 path_key = (optimal_path[0], optimal_path[-1])
-
 path_nodes = get_nodes_by_ids(driver, optimal_path)
-
 new_path = {path_key: path_nodes}
 
 # Crear el mapa con el nuevo camino
 graph_map.create_graph_map_single_color(new_path)
-
-
 
 
 driver.close()
