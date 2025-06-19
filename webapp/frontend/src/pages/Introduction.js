f8d20bee19569a0d1f64765fca27445dce46300c
gabichulas <gabylopezromero13@gmail.com>
2025-06-18 21:02:05 -0300

    login

--- webapp/frontend/src/pages/Introduction.js
+++ webapp/frontend/src/pages/Introduction.js
@@ -1,13 +1,21 @@
+import { useNavigate } from "react-router-dom";
+
 export default function Introduction() {
-    return (
-      <section className="ml-64 p-6">
-        <h2 className="text-3xl font-bold mb-4">Introducción</h2>
-        <p className="text-lg text-gray-700">
-          Este sistema permite ubicar puntos en el mapa como locales o centros de distribución
-          y calcular rutas óptimas entre ellos utilizando algoritmos de optimización.
-          Los datos de los puntos provendrán de una API externa.
-        </p>
-      </section>
-    );
-  }
-  
+  const navigate = useNavigate();
+  return (
+    <section className="ml-64 p-6">
+      <h2 className="text-3xl font-bold mb-4">Introducción</h2>
+      <p className="text-lg text-gray-700">
+        Este sistema permite ubicar puntos en el mapa como locales o centros de distribución
+        y calcular rutas óptimas entre ellos utilizando algoritmos de optimización.
+        Los datos de los puntos provendrán de una API externa.
+      </p>
+      <button
+        className="mt-8 bg-blue-600 text-white px-6 py-2 rounded hover:bg-blue-700 font-semibold"
+        onClick={() => navigate("/login")}
+      >
+        Iniciar sesión
+      </button>
+    </section>
+  );
+}
